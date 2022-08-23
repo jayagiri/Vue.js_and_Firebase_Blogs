9faedd243b906e329623cbae6eaf493211913668
Bijaya Giri <jaya.jc999@gmail.com>
2022-08-23 17:11:17 +0545

    Blogcard update (#3)
    * footer added
    
    * blogPost bug fix and update
    
    * blogPost image not loading bug fixed
    
    * blogCard featuer added with CSS optimized
    
    Co-authored-by: Bijaya Giri <bijayagiri@Bijayas-MacBook-Pro.local>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,128 @@
 <template>
   <div class="home">
-   <BlogPost :post="welcomeScreen" /> 
-   <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key ="index" /> 
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards
+            :post="post"
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a update. Register for your free account today !</h2>
+        <router-link class="router-button" to="#">
+          Register for fireBlog <Arrow />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import BlogPost from "../components/BlogPost"
+import BlogPost from "../components/BlogPost";
+import BlogCards from "../components/BlogCard";
+import Arrow from "../assets/Icons/arrow-right-light.svg";
 export default {
   name: "Home",
-  components: {BlogPost},
-  data () {
+  components: { BlogPost, BlogCards, Arrow },
+  data() {
     return {
       welcomeScreen: {
-        title: "Welcome",
-        blogPost: 
-        "Weekly blog post containes all programming line CSS, HTML , CSS ",
+        title: "Welcome!",
+        blogPost:
+          "Weekly blog post containes all programming line CSS, HTML , CSS also lots of new deatils to be ssen again and again. so keep tuned in guys",
         welcomeScreen: true,
-        photo:"coading",
+        photo: "coding",
       },
       sampleBlogPost: [
         {
           title: "This is a filter",
           blogHTML: "This is a filter blog post title",
           blogCoverPhoto: "beautiful-stories",
-
         },
         {
-        title: "This is filter title",
-        blogHTML: "This is filter blog post title !",
-        blogCoverPhoto: "designed-for-everyone",
+          title: "This is filter title",
+          blogHTML: "This is filter blog post title",
+          blogCverPhoto: "designed-for-everyone",
+        },
+      ],
 
-        }
-      ]
-    }
-  }
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "May 1, 2021",
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;  
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
