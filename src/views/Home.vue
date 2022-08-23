2f181aa51f46821b40c390369418e7528a2fcdca
Bijaya Giri <jaya.jc999@gmail.com>
2022-08-23 17:07:05 +0545

    Blog post update (#2)
    * footer added
    
    * blogPost bug fix and update
    
    Co-authored-by: Bijaya Giri <bijayagiri@Bijayas-MacBook-Pro.local>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="home">
-   
+   <BlogPost :post="welcomeScreen" /> 
+   <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key ="index" /> 
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost"
 export default {
   name: "Home",
-  components: {},
+  components: {BlogPost},
+  data () {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost: 
+        "Weekly blog post containes all programming line CSS, HTML , CSS ",
+        welcomeScreen: true,
+        photo:"coading",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a filter",
+          blogHTML: "This is a filter blog post title",
+          blogCoverPhoto: "beautiful-stories",
+
+        },
+        {
+        title: "This is filter title",
+        blogHTML: "This is filter blog post title !",
+        blogCoverPhoto: "designed-for-everyone",
+
+        }
+      ]
+    }
+  }
 };
 </script>
